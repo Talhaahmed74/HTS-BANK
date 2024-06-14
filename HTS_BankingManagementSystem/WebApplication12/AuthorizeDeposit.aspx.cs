2225c59e0bc8e0251228a3508c9a6a3bd93d5ddb
hassaangulrao <hassaangulrao8@gmail.com>
2024-06-14 17:40:18 +0500

    initial commit

---
+++ HTS_BankingManagementSystem/WebApplication12/AuthorizeDeposit.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using BusinessLogicLayer;
+
+namespace WebApplication12
+{
+    public partial class AuthorizeDeposit : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ProceedButton_Click(object sender, EventArgs e)
+        {
+            int accountNumberValue;
+            decimal amountValue;
+
+            // Access the Text property from the TextBox controls
+            string accountNumberText = accountNumber.Text;
+            string amountText = amount.Text;
+
+            if (int.TryParse(accountNumberText, out accountNumberValue) && decimal.TryParse(amountText, out amountValue))
+            {
+                AuthorizeDepositBL authorizeDepositBL = new AuthorizeDepositBL();
+                bool success = authorizeDepositBL.DepositAmount(accountNumberValue, amountValue);
+
+                if (success)
+                {
+                    // After processing, clear the text fields
+                    accountNumber.Text = "";
+                    amount.Text = "";
+
+                    // Display a message indicating that the deposit is done
+                    ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Deposit done!');", true);
+                }
+                else
+                {
+                    ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Failed to process deposit.');", true);
+                }
+            }
+            else
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Invalid input.');", true);
+            }
+        }
+    }
+}
